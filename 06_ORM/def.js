d8c4e0aba696a2afe9aa73a23dad19b6fcc13248
Hit Tusharbhai Narola <kanonarola8@gmail.com>
2026-02-08 05:18:19 +0000

    Add ORM docker compose and notes

---
+++ 06_ORM/def.js
@@ -0,0 +1,13 @@
+// ORM -> is the object relational mapping thant convert the object into the relational database to understand the object 
+// to connect with database we have to download the ORM tool in out machine to connect with database
+// to connect javascript with postgres and js-->mysql we install drizzle orm
+//js--> mongodb we install mongoose orm
+
+// python--> postgres and mysql --> we use sqlalchemy orm
+
+// prisma orm  which is used to connect js to any database like postgres , mysql ,sqlite etc
+
+//three method of download
+//1 full download through website of postgres
+// 2. through remote server (cloud database) means we can use the online database like supabase 
+//3. through docker container means we can use the database in our local machine without installing it in other words we used it virtually
